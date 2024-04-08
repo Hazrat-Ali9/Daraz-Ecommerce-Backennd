a95eb576c4064adbc4c9e31d76b301c70c8cb116
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-04-08 23:28:35 +0600

    Dashboard :racehorse:

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ async function run() {
             }
         });
         
-        // Regular user dashboard
+        // Regular user Dashboard
         app.get("/user/dashboard", async (req, res) => {
             res.send("Welcome to user dashboard!");
         });
